30b9e1e87b2cfdb50e5ab4cefb15207d632b9707
edgloriam <edgloriamm@gmail.com>
2017-07-24 12:04:44 +0300

    create relations between books and authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717073801) do
+ActiveRecord::Schema.define(version: 20170721110049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authors_books", id: false, force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "book_id"
+    t.index ["author_id", "book_id"], name: "index_authors_books_on_author_id_and_book_id", using: :btree
+    t.index ["author_id"], name: "index_authors_books_on_author_id", using: :btree
+    t.index ["book_id"], name: "index_authors_books_on_book_id", using: :btree
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string   "name"
+    t.string   "photo"
+    t.string   "description"
+    t.float    "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "books_authors", force: :cascade do |t|
+    t.integer "book_id"
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_books_authors_on_author_id", using: :btree
+    t.index ["book_id"], name: "index_books_authors_on_book_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +62,6 @@ ActiveRecord::Schema.define(version: 20170717073801) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "books_authors", "authors"
+  add_foreign_key "books_authors", "books"
 end
