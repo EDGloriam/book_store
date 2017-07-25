3f015690011b024de99ea3f1fd5c30e10d9a47a6
edgloriam <edgloriamm@gmail.com>
2017-07-25 15:05:10 +0300

    add style to log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root 'devise/sessions#new'
+  resources :books
+  root 'books#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
