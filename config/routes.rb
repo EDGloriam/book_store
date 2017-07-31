f96ea35d1369f6d32016730eca0afeddd16cd0af
edgloriam <edgloriamm@gmail.com>
2017-07-31 14:30:32 +0300

    temp

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-
-  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
-  resources :books
   root 'pages#index'
   get '/users/auth/facebook/callback', to: 'pages#index'
-
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-# , controllers: {omniauth_callbacks: 'omniauth_callbacks'}
