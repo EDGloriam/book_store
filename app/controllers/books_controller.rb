816ab625e7338ca4fee8058dda1284d00d63fd47
EDGloriam <edgloriamm@gmail.com>
2017-08-03 15:45:59 +0300

    temp

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,14 @@
 class BooksController < ApplicationController
-  def index   # GET /posts
+  def index   # GET /books
     @batch_of_books = []
     Book.find_each(batch_size: 8) do |book|
       @batch_of_books << book
     end
   end
 
-  def show    # GET /posts/:id
-    # ...
+  def show    # GET /books/:id
+    @book = Book.find_by(id: params[:id])
+    puts @book
   end
 
   def new     # GET /posts/new
