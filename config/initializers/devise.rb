08266591e237f4ad58fbc1a888d3a34e35ca3a1c
edgloriam <edgloriamm@gmail.com>
2017-10-20 03:08:59 +0300

    Allowed Unconfirmed Access

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,7 +16,7 @@ Devise.setup do |config|
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'MyMailer'
-
+  config.allow_unconfirmed_access_for = 365.day
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
 
