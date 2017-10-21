84bac8c735c051a713638364d9099d4398ee0f63
edgloriam <edgloriamm@gmail.com>
2017-10-21 17:43:44 +0300

    fix1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   #Added by myself
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
 
 
 
