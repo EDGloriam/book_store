dcf696a149677a32496d00b7c2d5b4fdd672ac6e
edgloriam <edgloriamm@gmail.com>
2017-11-02 16:11:49 +0200

    fix mail confirmation. move  (require dotenv /  Dotenv.load) to config.ru

--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
+require 'dotenv'
 
 run Rails.application
+Dotenv.load
--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,14 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-require 'dotenv'
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
-Dotenv.load
+
+
 
 module Bookstore
   class Application < Rails::Application
