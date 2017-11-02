ce59a6e62cdd7ef76ded2a6234900c1065fb66b4
edgloriam <edgloriamm@gmail.com>
2017-11-02 15:48:46 +0200

    fix mail confirmation. add (require dotenv /  Dotenv.load)

--- config/application.rb
+++ config/application.rb
@@ -10,25 +10,20 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require 'dotenv'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+Dotenv.load
 
 module Bookstore
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    ActionMailer::Base.smtp_settings = {
-      authentication: :plain,
-      address: 'smtp.mailgun.org',
-      port: 587,
-      domain: 'bookstore-shop.herokuapp.com',
-      user_name: 'postmaster@bookstore-shop.herokuapp.com',
-      password: 'e3ba7c2efb23526de5a497874882ae20'
-    }
 
   end
 end
