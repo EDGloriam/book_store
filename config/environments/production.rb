18e8f774151aaabaf20680d02b9ef0969b5a0637
edgloriam <edgloriamm@gmail.com>
2017-11-02 15:04:00 +0200

    fix mail confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,21 +25,22 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-
-
-  #Added by meself
-  config.action_mailer.default_url_options = { host: 'bookstore-shop.herokuapp.com' }
-  # config.action_mailer.smtp_settings = {
-  #     address: 'smtp.gmail.com',
-  #     domain: 'mail.google.com',
-  #     port: 587,
-  #     user_name: ENV['GMAIL_USERNAME'],
-  #     password: ENV['GMAIL_PASSWORD'],
-  #     authentication: 'plain',
-  #     enable_starttls_auto: true
-  #   }
-  config.action_mailer.delivery_method = :smtp
+  #Added by myself
+  config.action_mailer.default :charset => 'utf-8'
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { host: 'bookstore-shop.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'bookstore-shop.herokuapp.com'
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    authentication: :plain,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    enable_starttls_auto: true
+  }
 
 
 
