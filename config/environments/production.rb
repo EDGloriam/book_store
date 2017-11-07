4b9784fc0e9be10ea7bd9af30191bb84d9c26166
edgloriam <edgloriamm@gmail.com>
2017-11-07 06:40:19 +0200

    fix1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.default_url_options = { host: 'bookstore-shop.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'bookstore-shop.herokuapp.com'
+  config.action_mailer.default_url_options = { host: 'book-stor.herokuapp.com' }
+  Rails.application.routes.default_url_options[:host] = 'book-stor.herokuapp.com'
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     domain: 'gmail.com',
